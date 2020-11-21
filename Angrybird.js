ed91d92332e51bec111ccd2a3897a34359e45004
HarshitBrar <73597686+HarshitBrar@users.noreply.github.com>
2020-11-21 19:58:33 +0530

    Add files via upload

---
+++ Angrybird.js
@@ -0,0 +1,27 @@
+class Angrybird{
+    constructor(x,y){
+        var options = {
+            restitution : 0.5
+        }
+        this.body = Bodies.rectangle(x,y,50,50,options);
+        this.width = 50;
+        this.height = 50;
+        World.add(myWorld,this.body);
+        
+    } 
+    display(){
+        var pos = this.body.position
+        pos.x = mouseX
+        pos.y = mouseY
+        var angle = this.body.angle
+        push()
+        translate(pos.x,pos.y)
+        rotate(angle)
+        rectMode(CENTER);
+        strokeWeight(4);
+        stroke("black");
+        fill("red");
+        rect(0,0,this.width,this.height);
+        pop()
+    } 
+}
